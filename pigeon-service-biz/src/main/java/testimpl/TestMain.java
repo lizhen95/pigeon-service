cec24ffc960353a4d8e9bf3870a2324522d99427
lizhen <lizhn95@163.com>
2017-11-26 09:52:30 +0800

    优化配置

--- pigeon-service-biz/src/main/java/testimpl/TestMain.java
+++ pigeon-service-biz/src/main/java/testimpl/TestMain.java
@@ -1,13 +1,26 @@
 package testimpl;
 
-import com.pigeon.module.test.service.TestService;
-import com.pigeon.module.test.service.impl.TestServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.pigeon.module.test.dto.TestDTO;
+import com.pigeon.module.test.service.TestService;
+
+@RestController
+@RequestMapping("/test")
 public class TestMain {
 
-	public static void main(String[] args) {
-		TestService tt = new TestServiceImpl();
-		System.out.println(tt.selectByPrimaryKey(1).toString());
+	@Autowired
+	TestService testService;
+	
+	@RequestMapping(value = "/showTest", method = { RequestMethod.POST })
+	public TestDTO showTest(@RequestBody TestDTO request){
+		int id = request.getId();
+		TestDTO t = testService.selectByPrimaryKey(id);
+		return t;
 	}
 
 }
